ecffad22c00f1db095ea22ce2b37ac4deb11f2b0
XiMucoding <91119649+XiMucoding@users.noreply.github.com>
2022-06-19 17:03:37 +0800

    python微博爬虫代码

--- weibo.py
+++ weibo.py
@@ -53,9 +53,9 @@ for item in bs.find_all("div", class_="row"):
 #连接数据库
 conn = pymysql.connect(host = '127.0.0.1' # 连接名称，默认127.0.0.1
 ,user = 'root' # 用户名
-,passwd='lizhikun' # 密码
+,passwd= # 密码
 ,port= 3306 # 端口，默认为3306
-,db= 'vueblog' # 数据库名称
+,db= # 数据库名称
 ,charset='utf8' # 字符编码
 )
 cur = conn.cursor() # 生成游标对象
